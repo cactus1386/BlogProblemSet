ca77262b51d6e83d2983e8d103bee99424969463
cactus1386 <cactusinjast@gmail.com>
2024-02-15 01:15:12 -0800

    make models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # TODO write all of your code here...
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)

5a40e202f929be21df7e9957b0d254d4c1c860cc
cactus1386 <cactusinjast@gmail.com>
2024-02-15 01:25:39 -0800

    make copy func

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # TODO write all of your code here...
 class Author(models.Model):
@@ -9,6 +10,20 @@ class BlogPost(models.Model):
     body = models.TextField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def copy(self):
+        blog = BlogPost.objects.get(pk=self.pk)
+        blog.id = None
+        blog.date_created = timezone.now()
+        blog.save()
+
+        comments = self.commet_set.all()
+        for comment in comments:
+            comment.id = None
+            comment.blog_post = blog
+            comment.save()
+
+        return blog.id
 
 class Comment(models.Model):
     blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
